878e510ffdf29a7bf3d0a8e6003132361546fd9b
RagingCitrusTree <stephanyandrea16@gmail.com>
2017-04-09 22:16:57 -0400

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-fn main() {
-    let x: i32;
+use std::io;
 
-    println!("The value of x is: {}", x);
+fn main() {
+    println!("What is your first name?");
+
+    let mut f_name = String::new();
+
+    io::stdin().read_line(&mut f_name)
+        .expect("Failed to read line");
+
+    println!("Your first name is {}. Is this correct?", f_name);
+    println!("y/n");
+
+
+}
+
+fn answer() {
+    let mut ans = String::new();
+
+    io::stdin().read_line(&mut ans)
+        .expect("Failed to read line")
 }
